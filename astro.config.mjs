1fd1693017801bea9009ad3e2e0607285e887b2b
Arisay <123779947+Arisayyy@users.noreply.github.com>
2026-07-02 20:27:14 -0600

    Use readable glyph atlas for CRT shader

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,9 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   output: 'server',
   adapter: cloudflare(),
+  vite: {
+    optimizeDeps: {
+      exclude: ['astro/compiler-runtime'],
+    },
+  },
 });

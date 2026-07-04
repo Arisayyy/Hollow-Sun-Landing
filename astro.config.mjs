c955893aa1c846b2d7d11f1f16269a6f2a32b31a
Arisay <123779947+Arisayyy@users.noreply.github.com>
2026-07-04 12:39:07 -0600

    chore: set site url to hollowsun.ai

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import cloudflare from '@astrojs/cloudflare';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  site: 'https://hollowsun.ai',
   output: 'server',
   adapter: cloudflare(),
 });
+
